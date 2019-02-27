7a9315bd86b0ab97acf8dea6781c26df258741ec
Adam Chen <adamtp_chen@walsin.com>
2019-02-27 13:22:32 +0800

    Create __init__.py

---
+++ wex-python-api/ibmwex/apis/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .collection_api import CollectionApi
+from .converter_api import ConverterApi
+from .crawler_api import CrawlerApi
+from .dataset_api import DatasetApi
+from .document_export_api import DocumentExportApi
+from .enrichment_api import EnrichmentApi
+from .exploration_api import ExplorationApi
+from .file_resource_api import FileResourceApi
+from .group_api import GroupApi
+from .importer_api import ImporterApi
+from .labeler_api import LabelerApi
+from .migration_api import MigrationApi
+from .nlp_api import NLPApi
+from .ranker_api import RankerApi
+from .similar_document_ranker_api import SimilarDocumentRankerApi
+from .system_api import SystemApi
+from .user_api import UserApi
